40c6c89bd7f23a5f2a5564fd3c5f68285238ad63
Phil Hartley <philh725@gmail.com>
2018-12-30 20:53:10 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,6 @@
 class Hash
   def keys_of(arguments)
-    if arguments = 'Paname'
+    if arguments = 'Panama'
       ["red-footed tortoise"]
     elsif arguments = 'Madagascar'
       ["aye-aye", "tomato frog"]
